d3eeec62355d2a7ef7385747b66ed3693458bb8b
doheeh <gdhee7597@gmail.com>
2024-12-28 16:57:27 +0900

    springboot 개발 환경 설정 3-1

---
+++ springboot_project/settingweb_boot/src/test/java/org/devfun/settingweb_boot/SettingwebBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devfun.settingweb_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettingwebBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
